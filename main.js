ff672afd07655e7e4dc2b318fcf24a00cdf4087b
Etasha2345 <71700664+Etasha2345@users.noreply.github.com>
2021-05-09 19:41:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ function draw() {
     image(video, 0, 0, 380, 380);
     if (status != "") {
         r = random(255);
-        g = ramdom(255);
-        b = ramdom(255);
+        g = random(255);
+        b = random(255);
         objectDetector.detect(video, gotResult);
         for (i = 0; i < objects.length; i++) {
             document.getElementById("status").innerHTML = "Status : Object Detected";
